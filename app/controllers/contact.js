74526705557139912ce2e81e3965d92a544f2937
KimSaeHun <kimsaehun@csu.fullerton.edu>
2018-04-21 11:19:30 -0700

    Complete contact page

---
+++ app/controllers/contact.js
@@ -0,0 +1,28 @@
+import Controller from '@ember/controller';
+import {
+  match,
+  not,
+  and,
+  gte
+} from '@ember/object/computed';
+
+export default Controller.extend({
+
+  message: '',
+  emailAddress: '',
+
+  isValidEmail: match('emailAddress', /^.+@.+\..+$/),
+  isValidMessage: gte('message.length', 5),
+  isValid: and('isValidEmail', 'isValidMessage'),
+  isDisabled: not('isValid'),
+
+  actions: {
+
+    sendMessage() {
+      alert('You\'re message has been sent!');
+      this.set('message', '');
+      this.set('emailAddress', '');
+    }
+  }
+
+});
